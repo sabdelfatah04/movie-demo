d9e65f676c96cb49ee11c84e199bf2ce2499e36d
sabdelfatah04 <50589044+sabdelfatah04@users.noreply.github.com>
2020-01-10 11:44:34 -0500

    1/10/19

--- src/routers/movies.js
+++ src/routers/movies.js
@@ -27,6 +27,34 @@ router.get("/movies/:id",  async (req, res) => {
     }catch (error) {
         res.status(500).send(error);
     }
-})
+});
+router.delete("/movies/:id", async(req, res) => {
+    try {
+        const movie = await Movie.findByIdAndDelete(req.params.id);
+        res.send(movie);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
 
+router.patch("/movies/:id", async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ["genre"];
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update)
+    );
+    try {
+        const movie = await Movie.findByIdAndUpdate(req.params.id, req.body);
+        res.send(movie);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+router.get("/movies",  async (req, res) => {
+    try {
+        const movie = await Movie.find({ year: { $gte: 2000, $lte: 2020 } });
+      res.send(movie);  
+    }catch (error) {
+        res.status(500).send(error);
+    }
+});
 module.exports = router;
